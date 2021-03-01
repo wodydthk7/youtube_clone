360319d452643326555f1ee0476fab466bdea527
정재용 <simon@jeongjaeyong-ui-MacBookPro.local>
2021-03-01 17:29:31 +0900

    fetch to axios at web communication

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,41 +1,83 @@
 // youtube api를 이용한 네트워크 통신 클래스
+import axios from 'axios'
 
 class Youtube {
   constructor(key) {
-    this.key = key
-    this.requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    this.youtube = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3/',
+      params: {key: key}
+    })
   }
 
-  async mostPopular() {
-    const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=10&key=${this.key}`
+  // using fetch
+  // constructor(key) {
+  //   this.key = key
+  //   this.requestOptions = {
+  //     method: 'GET',
+  //     redirect: 'follow'
+  //   };
+  // }
 
+  async mostPopular() {
+    // axios instance method는 promise를 리턴함
     try {
-      const response = await fetch(url, this.requestOptions);
-      const result = await response.json();
-      return result.items;
+      const response = await this.youtube.get('videos', {
+        params: {
+          part: 'snippet',
+          chart: 'mostPopular',
+          regionCode: 'KR',
+          maxResults: 20,
+        }
+      })
+      return response.data.items;
     } catch (error) {
       return console.log('errer', error);
     }
+
+    
+    // using fetch
+    // const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=10&key=${this.key}`
+
+    // try {
+    //   const response = await fetch(url, this.requestOptions);
+    //   const result = await response.json();
+    //   return result.items;
+    // } catch (error) {
+    //   return console.log('errer', error);
+    // }
   }
 
   async search(query) {
     if(query === "") {
       return this.mostPopular()
     }
-
-    const url = `https://www.googleapis.com/youtube/v3/search?q=${query}&maxResults=25&part=snippet&type=video&key=${this.key}`
-
+    
     try {
-      const response = await fetch(url, this.requestOptions);
-      const result = await response.json();
-      return result.items.map(item => ({ ...item, id: item.id.videoId }));
+      const response = await this.youtube.get('search', {
+        params: {
+          q: query,
+          maxResults: 25,
+          part: 'snippet',
+          type: 'video'
+        }
+      })
+      return response.data.items.map(item => ({ ...item, id: item.id.videoId }));
     } catch (error) {
       return console.log('error', error);
     }
   }
+
+  // using fetch
+  //   const url = `https://www.googleapis.com/youtube/v3/search?q=${query}&maxResults=25&part=snippet&type=video&key=${this.key}`
+
+  //   try {
+  //     const response = await fetch(url, this.requestOptions);
+  //     const result = await response.json();
+  //     return result.items.map(item => ({ ...item, id: item.id.videoId }));
+  //   } catch (error) {
+  //     return console.log('error', error);
+  //   }
+  // }
 }
 
 export default Youtube
